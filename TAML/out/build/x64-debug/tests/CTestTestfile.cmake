1b2586dda270b3793ec339147063f866b5b95dcf
Tomer Steinberg <tomerst77@gmail.com>
2025-01-04 21:16:31 +0200

    reorder file structure

--- TAML/out/build/x64-debug/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Tomer/OneDrive/Desktop/TAML/TAML/tests
-# Build directory: C:/Users/Tomer/OneDrive/Desktop/TAML/TAML/out/build/x64-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
